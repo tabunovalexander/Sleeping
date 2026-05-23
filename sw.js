b9ef6c73dc67a8949ed10f3bb578227ccdeb482a
tabunovalexander <tabunov.alexander@gmail.com>
2026-05-23 15:42:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'sleep-app-v1';
+const ASSETS = ['index.html'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
+});
